9cd3036d3bb3b6e80270925a73f965af6e0b49d6
g5n9u1-coder <shashank.singh2_cs.aiml@gla.ac.in>
2026-02-27 17:24:16 +0530

    Initialize Serial communication (9600 baud rate)

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ void setup() {
 
     // TODO 4:
     // Initialize Serial communication (9600 baud rate)
-  //  Serial.begin(9600);
+    Serial.begin(9600);
 
     // TODO 5:
     // Initialize the DHT sensor
